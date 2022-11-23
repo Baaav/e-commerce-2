52e1583fbaa810f2dacbd05cccdc383db7b7f281
bavlybadry123 <bavlybadry123@gmail.com>
2022-11-23 23:22:02 +0200

    create icon and text in appbar

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,25 +6,39 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: appbarGreen,
-        title: Text("Home"),
-        actions: [
-          Row(
-            children: [
-              Text(
-                "\$13",
-                style: TextStyle(fontSize: 18),
-              ),
-              IconButton(onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("\$13"),
-              )
-            ],
-          ),
-        ],
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: appbarGreen,
+          title: Text("Home"),
+          actions: [
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      child: Text(
+                        "8",
+                        style: TextStyle(
+                            fontSize: 18, color: Color.fromARGB(255, 0, 0, 0)),
+                      ),
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                          color: Color.fromARGB(211, 164, 255, 193),
+                          shape: BoxShape.circle),
+                    ),
+                    IconButton(
+                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 12.0),
+                  child: Text("\$13"),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
