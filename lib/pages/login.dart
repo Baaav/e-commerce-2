17a4853138297d7bfa15bf3063caee656b893fd5
bavlybadry123 <bavlybadry123@gmail.com>
2022-11-23 17:44:06 +0200

    create button and textbutton

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -11,6 +11,7 @@ class Login extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(33.0),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
                 height: 164,
@@ -29,6 +30,33 @@ class Login extends StatelessWidget {
                 obscureText: true,
                 decoration: decorationtextfield.copyWith(
                     hintText: " Enter Your Password "),
+              ),
+              const SizedBox(
+                height: 64,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(12)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+                ),
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(fontSize: 19),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don not have an account ? "),
+                  TextButton(
+                    onPressed: () {},
+                    child:
+                        Text('sign up', style: TextStyle(color: Colors.black)),
+                  )
+                ],
               )
             ],
           ),
