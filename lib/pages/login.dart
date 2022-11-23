15e1eb18adad6c52f0eb513faabaf1a857782d84
bavlybadry123 <bavlybadry123@gmail.com>
2022-11-23 16:02:39 +0200

    design text field for log in page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -5,6 +5,51 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                obscureText: false,
+                decoration: InputDecoration(
+                    hintText: "Enter Your Email ",
+                    //delete porder :
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(8)),
+              ),
+              TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Enter Your Password ",
+                    //delete porder :
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: Divider.createBorderSide(context),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    contentPadding: EdgeInsets.all(8)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
