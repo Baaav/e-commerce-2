54a645945bc7324ba9a7997f5143c39780904ff0
bavlybadry123 <bavlybadry123@gmail.com>
2022-11-24 00:49:50 +0200

    create drawer and add picture and lists

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
-import 'package:ecommerce_2/shared/constants.dart';
+// ignore_for_file: prefer_const_constructors, duplicate_ignore
+
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -6,39 +7,93 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: appbarGreen,
-          title: Text("Home"),
-          actions: [
-            Row(
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
               children: [
-                Stack(
-                  children: [
-                    Container(
+                // ignore: prefer_const_constructors
+                UserAccountsDrawerHeader(
+                  // ignore: prefer_const_constructors
+                  decoration: BoxDecoration(
+                    image: const DecorationImage(
+                        image: AssetImage("assets/img/test.jpg"),
+                        fit: BoxFit.cover),
+                  ),
+                  accountName: const Text("ali Hassan",
+                      // ignore: prefer_const_constructors
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                      )),
+                  accountEmail: const Text("ali@yahoo.com"),
+                  currentAccountPictureSize: const Size.square(99),
+                  currentAccountPicture: const CircleAvatar(
+                      radius: 55,
+                      backgroundImage: AssetImage("assets/bav.jpg")),
+                ),
+                ListTile(
+                    title: const Text("Home"),
+                    leading: const Icon(Icons.home),
+                    onTap: () {}),
+                ListTile(
+                    title: const Text("My products"),
+                    leading: const Icon(Icons.add_shopping_cart),
+                    onTap: () {}),
+                ListTile(
+                    title: const Text("About"),
+                    leading: const Icon(Icons.help_center),
+                    onTap: () {}),
+                ListTile(
+                    title: const Text("Logout"),
+                    leading: const Icon(Icons.exit_to_app),
+                    onTap: () {}),
+              ],
+            ),
+            Container(
+              // ignore: prefer_const_constructors
+              margin: EdgeInsets.only(bottom: 12),
+              // ignore: prefer_const_constructors
+              child: Text("Developed by Bavly Hassan © 2022",
+                  // ignore: prefer_const_constructors
+                  style: TextStyle(fontSize: 16)),
+            )
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        // ignore: prefer_const_constructors
+        backgroundColor: Color.fromARGB(255, 76, 141, 95),
+        // ignore: prefer_const_constructors
+        title: Text("Home"),
+        actions: [
+          Row(
+            children: [
+              Stack(
+                children: [
+                  // ignore: duplicate_ignore
+                  Container(
+                      // ignore: prefer_const_constructors, sort_child_properties_last
                       child: Text(
                         "8",
-                        style: TextStyle(
-                            fontSize: 18, color: Color.fromARGB(255, 0, 0, 0)),
+                        style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
                       ),
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
                           color: Color.fromARGB(211, 164, 255, 193),
-                          shape: BoxShape.circle),
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: Text("\$13"),
-                )
-              ],
-            ),
-          ],
-        ),
+                          shape: BoxShape.circle)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.add_shopping_cart)),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 12),
+                child: Text("\$ 128"),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
