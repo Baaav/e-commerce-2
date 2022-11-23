9ab593428a7c326340c6f054fd701cc95b1b7fa9
bavlybadry123 <bavlybadry123@gmail.com>
2022-11-23 17:48:29 +0200

    create  Rigister page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ecommerce_2/pages/login.dart';
+import 'package:ecommerce_2/pages/rigister.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Login(),
+      home: Rigister(),
     );
   }
 }
--- lib/pages/rigister.dart
+++ lib/pages/rigister.dart
@@ -1,8 +1,8 @@
 import 'package:ecommerce_2/shared/constants.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+class Rigister extends StatelessWidget {
+  const Rigister({super.key});
 
   @override
   Widget build(BuildContext context) {
